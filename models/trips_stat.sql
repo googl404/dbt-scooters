93c6eed005167a739fc5443516222c976cff81d4
Максим Готовцев <magot.123@gmail.com>
2024-04-16 18:57:49 +0000

    рефакторинг trips_stat

--- models/trips_stat.sql
+++ models/trips_stat.sql
@@ -1,9 +1,10 @@
 select count(*) trips,
 count(DISTINCT user_id) users,
- AVG( extract(epoch from (finished_at - started_at))) / 60   avg_duration_m, 
-SUM(price)/100 revenue_rub,
-  sum( case when price>0 then 0 else 1 end)    /(count(*) :: real) *100  as  free_trips_pct
-  from scooters_raw.trips
+ AVG( duration_s) / 60   avg_duration_m, 
+SUM(price_rub)/100 revenue_rub,
+  sum( case when is_free is true or null then 0 else 1 end)    /(count(*) :: real) *100  as  free_trips_pct,
+  sum(distance_m)/1000 as sum_distance_km
+  from {{ ref("trips_prep") }}
   
 /* trips - суммарное количество поездок
 users - количество уникальных пользователей
