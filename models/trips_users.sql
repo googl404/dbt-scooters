7c9308e6a01d68358d478f69b270ec3b93928784
Максим Готовцев <magot.123@gmail.com>
2024-04-26 16:09:47 +0000

    trips_users

---
+++ models/trips_users.sql
@@ -0,0 +1,8 @@
+select
+    t_p.*, U.sex,   extract(year from t_p.started_at) - extract(year from u.birth_date) as age
+from
+    {{ ref('trips_prep') }} as t_p
+left join
+    scooters_raw.users as U
+ on t_p.user_id = u.id
+  
