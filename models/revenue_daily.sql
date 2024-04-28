b7aa954330552517e5a5245c93e03432fc888851
Максим Готовцев <magot.123@gmail.com>
2024-04-28 09:40:21 +0000

    revenue_daily incremental

---
+++ models/revenue_daily.sql
@@ -0,0 +1,13 @@
+select  
+date(started_at) as date ,
+sum(price_rub) revenue_daily
+from  {{ ref('trips_prep') }}
+
+{% if is_incremental() %}
+where date >= (select max(date)  - interval 2 days from  {{ this }})
+ 
+{% else %}  
+
+{% endif %}
+group by 1
+order by 1
